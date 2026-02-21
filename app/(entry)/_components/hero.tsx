154b125b0cf7b7f3007316209e48a5d1a742df35
mooshcat <akuzikefoxxmisomali@gmail.com>
2026-02-21 08:51:31 +0200

    revamped home page

--- app/(entry)/_components/hero.tsx
+++ app/(entry)/_components/hero.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Hero() {
   
   return (
-    <section className='w-full h-screen relative flex flex-col items-center justify-center'>
+    <section className='flex-1 w-full h-full relative flex flex-col items-center justify-center'>
       <section className='flex flex-col text-center w-3/4 relative gap-5 items-center justify-center'>
         <h2 className='text-5xl font-black'>HostelHub is the Complete Platform for Modern Hostel Living and Property Management</h2>
         <p className=''>Whether you`re a tenant searching for the right place to stay or a landlord managing rooms, payments, and announcements, HostelHub brings everything together in one seamless experience.</p>
--- app/(entry)/page.tsx
+++ app/(entry)/page.tsx
@@ -5,7 +5,7 @@ import Hero from './_components/hero'
 export default function page() {
 
     return (
-        <section className='w-full relative min-h-screen'>
+        <section className='w-full relative flex flex-col h-screen'>
             <Navigation />
             <Hero />
         </section>
